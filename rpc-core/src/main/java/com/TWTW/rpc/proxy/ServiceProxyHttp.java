6e6df2b945fd5510865ea2a7c7c4303ab043c8d0
TWTW <2952560241@qq.com>
2025-08-13 16:48:56 +0800

    7 自定义协议 http协议请求中无意义的数据较多，故而自己设计请求协议

---
+++ rpc-core/src/main/java/com/TWTW/rpc/proxy/ServiceProxyHttp.java
@@ -0,0 +1,82 @@
+package com.TWTW.rpc.proxy;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import com.TWTW.rpc.RpcApplication;
+import com.TWTW.rpc.config.RpcConfig;
+import com.TWTW.rpc.constant.RpcConstant;
+import com.TWTW.rpc.model.RpcRequest;
+import com.TWTW.rpc.model.RpcResponse;
+import com.TWTW.rpc.model.ServiceMetaInfo;
+import com.TWTW.rpc.registry.Registry;
+import com.TWTW.rpc.registry.RegistryFactory;
+import com.TWTW.rpc.serializer.Serializer;
+import com.TWTW.rpc.serializer.SerializerFactory;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * @author TWTW
+ * @version 1.0
+ * 描述：服务代理（JDK 动态代理）
+ * @date 2025/6/28 下午1:25
+ */
+
+
+public class ServiceProxyHttp implements InvocationHandler {
+
+    /**
+     * 调用代理
+     *
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 指定序列化器
+        final Serializer serializer = SerializerFactory.getInstance(RpcApplication.getRpcConfig().getSerializer());
+
+        // 构造请求
+        String serviceName = method.getDeclaringClass().getName();
+        RpcRequest rpcRequest = RpcRequest.builder()
+                .serviceName(serviceName)
+                .methodName(method.getName())
+                .parameterTypes(method.getParameterTypes())
+                .args(args)
+                .build();
+        try {
+            // 序列化
+            byte[] bodyBytes = serializer.serialize(rpcRequest);
+
+            // 从注册中心获取服务提供者请求地址
+            RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+            Registry registry = RegistryFactory.getInstance(rpcConfig.getRegistryConfig().getRegistry());
+            ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+            serviceMetaInfo.setServiceName(serviceName);
+            serviceMetaInfo.setServiceVersion(RpcConstant.DEFAULT_SERVICE_VERSION);
+            List<ServiceMetaInfo> serviceMetaInfoList = registry.serviceDiscovery(serviceMetaInfo.getServiceKey());
+            if (CollUtil.isEmpty(serviceMetaInfoList)) {
+                throw new RuntimeException("暂无服务地址");
+            }
+            ServiceMetaInfo selectedServiceMetaInfo = serviceMetaInfoList.get(0);
+
+            // 发送请求
+            try (HttpResponse httpResponse = HttpRequest.post(selectedServiceMetaInfo.getServiceAddress())
+                    .body(bodyBytes)
+                    .execute()) {
+                byte[] result = httpResponse.bodyBytes();
+                // 反序列化
+                RpcResponse rpcResponse = serializer.deserialize(result, RpcResponse.class);
+                return rpcResponse.getData();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
